ce9aed7a197dd6a57b2049e78175e30ca6ea6961
liuhaichao <106967850+Party-Never-Die@users.noreply.github.com>
2022-07-09 16:40:08 +0800

    Add files via upload

---
+++ build/CMakeFiles/fushi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fushi.dir/main.cpp.o"
+  "fushi.pdb"
+  "fushi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fushi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
